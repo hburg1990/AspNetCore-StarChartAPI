60692f7b5d608835af3b6a6379de68cad8e1ecc6
Harry Burgess <hburgess1990@gmail.com>
2020-12-18 20:26:16 +0000

    Fix

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -35,18 +35,21 @@ namespace StarChart.Controllers
         [HttpGet("{name}")]
         public IActionResult GetByName(string name)
         {
-            var celestialObject = _context.CelestialObjects.FirstOrDefault(c => c.Name == name);
-            if (celestialObject == null)
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Name == name);
+            if (!celestialObjects.Any())
             {
                 return NotFound();
             }
 
-            celestialObject.Satellites = 
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satellites = 
                 _context.CelestialObjects
                     .Where(c => c.OrbitedObjectId == celestialObject.Id)
                     .ToList();
+            }
 
-            return Ok(celestialObject);
+            return Ok(celestialObjects);
         }
 
         [HttpGet]
